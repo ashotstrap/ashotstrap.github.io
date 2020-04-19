dfc2e3ff1c6cdb7beb19b360f3d5282311c84217
Ashot Uzunyan <54761976+ashotstrap@users.noreply.github.com>
2020-04-19 11:34:27 +0400

    Add files via upload

---
+++ precache-manifest.43a7ca5db072810cb65500fa623e997d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b489a647cf43e7cd4d2c1e127e0d620",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c51e403b5abbd7337f6",
+    "url": "/static/css/main.79d7e2b2.chunk.css"
+  },
+  {
+    "revision": "ba035831762388bad1d0",
+    "url": "/static/js/2.6d711b1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6d711b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c51e403b5abbd7337f6",
+    "url": "/static/js/main.e7a1324a.chunk.js"
+  },
+  {
+    "revision": "7745b6d279838917fb23",
+    "url": "/static/js/runtime-main.5e0a3024.js"
+  }
+]);
